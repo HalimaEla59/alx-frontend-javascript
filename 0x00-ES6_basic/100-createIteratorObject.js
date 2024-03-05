4629c729b4d1424fcebcc3a27446c10d9f3339d8
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-03-05 02:13:49 +0100

    adding 100-createIteratorObject.js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const i of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...i,
+    ];
+  }
+  return allEmployees;
+}
