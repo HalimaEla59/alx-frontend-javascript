d93bc292c130743c0c5e86bc104068890bd06479
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-03-12 23:46:02 +0000

    style fix 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,3 @@
 export default function updateStudentGradeByCity(studentlist, city, newGrades) {
-  return studentlist.filter((student) => student.location === city).map((student) => ({
-      ...student,
-      grade: newGrades.filter((g) => g.studentId === student.id).map((g) => g.grade)[0] || 'N/A'
-    }));
+  return studentlist.filter((student) => student.location === city).map((student) => ({ ...student, grade: newGrades.filter((g) => g.studentId === student.id).map((g) => g.grade)[0] || 'N/A' }));
 }
